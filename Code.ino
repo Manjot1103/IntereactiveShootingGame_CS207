5a04c94bee583f726f0fbf05f8160b6ceb3045e4
Manjot1103 <79737374+Manjot1103@users.noreply.github.com>
2021-04-17 06:27:15 -0600

    Update and rename Project.ino to Code.ino

--- Code.ino
+++ Code.ino
@@ -15,45 +15,60 @@ using namespace admux;
 
 //Array for 4 servos
 Servo servo[4];
-//Array storing the pins for servo
+//Array storing the 4 pins for servo
 const byte servoPins[4] = {2,3,4,5};
-//
+//Multiplexer object with d(ata pin, input type, pintype)  and Pinset for control pins. 
 Mux mux(Pin(A0, INPUT, PinType::Analog), Pinset(8, 9, 10, 11));
+//Array for storing readings from 4 knock sensor (piezo element)
 int sensor[4] = {0,0,0,0};
+// Array to store the previous position (angle value) of the 4 servos.
 int old[4] = {78,78,78,78};
+// Array to store the current position (angle value) of the 4 servos.
 int pos[4]= {78,78,78,78};
+// Array to store the score of 2 players
 int player[2] = {0,0};
 
+// LiquidCrystal_I2C class object for the lcd module with parameters (lcd address, columns, rows) 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-
+// timer threshold of value 20 (it is the duration for which the game will run i.e 20 secs)
 long timerThreshold = 20;
+// sensor threshold of value 50 (it is the value for which the senor will trigger the servo)
 byte sensorThreshold = 50;
+// variable to keep record of the previous version of a value in code for comparison.
 int prevRan;
 
 void setup() {
-
+  //calling the lcd method to trun on the lcd module
   lcd.begin();
+  //calling the backlight method to turn on the lcd backlight
   lcd.backlight();
+  //Setting the serial baud rate at 9600
   Serial.begin(9600);
 
+  //loop to attach the 4 pins to the servos and setting then at an angle of 78
   for(int  i = 0 ; i < 4 ; i++)
   {
     servo[i].attach(servoPins[i]);
     servo[i].write(78);
   }
   
-  
+  // printing to the lcd at (0,0)
   lcd.print("      WELCOME!      ");
+  // setting lcd cursor to row 0 and column 1
   lcd.setCursor(0,1);
   lcd.print("  TO SHOOTING GAME  ");
+  // delay of 2600 milliseconds
   delay(2600);
-
+  
+  // setting all the 4 servos at an angle of 3
     for(int  i = 0 ; i < 4 ; i++)
   {
     servo[i].write(3);
   }
+  // delay of 1000 milliseconds
   delay(1000);
+  // setting all the 4 servos at an angle of 78
     for(int  i = 0 ; i < 4 ; i++)
   {
     servo[i].write(78);
@@ -61,11 +76,10 @@ void setup() {
 
 
   /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+  // Game code 
   /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-  
-
-
+  //  
   for(int i = 0 ; i < 2 ; i++)
   {
     
